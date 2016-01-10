52b646ae5c4bf4b0e80b1c8214124bbd8e4fe1cd
zeglor <zeglor@gmail.com>
2016-01-10 18:20:25 +0300

    Fixed media and static settings

--- babyshop/babyshop/settings.py
+++ babyshop/babyshop/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
@@ -135,8 +136,8 @@ STATICFILES_DIRS = (
 
 # Media
 
-MEDIA_ROOT = '/home/zeglor/Documents/babyshop/media/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Tell browsers not to guess content type
 SECURE_CONTENT_TYPE_NOSNIFF = True
