f384bb239ef5d973a789fe6e36b8775e966752a2
zeglor <zeglor@gmail.com>
2016-01-17 20:53:31 +0300

    quick fix utls.py

--- babyshop/cart/urls.py
+++ babyshop/cart/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 
-from .views import add_to_cart, view_cart, add_to_cart_async
+from .views import view_cart, add_to_cart_async
 
 urlpatterns = [
 	# ex: /cart/add_async/
